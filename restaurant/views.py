a6c9199526e9034bf64b1f3430d27bc864b93094
DonPires94 <dom.pires94@gmail.com>
2023-05-10 17:59:53 +0100

    Created serializers for Booking + Menu Models

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+
 
 def index(request):
     return render(request, "index.html", {})
